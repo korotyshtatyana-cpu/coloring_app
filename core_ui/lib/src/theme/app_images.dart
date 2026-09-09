6dba10d36f4bce2a278781fb3c7b173e32bf99aa
“Tatyana <korotyshtatyana@gmail.com>
2026-09-09 21:50:06 +0300

    add app icon

--- core_ui/lib/src/theme/app_images.dart
+++ core_ui/lib/src/theme/app_images.dart
@@ -3,9 +3,8 @@ import '../constants/package_constants.dart';
 class AppImages {
   static const String packageName = PackageConstants.kPackageName;
 
-  static const String _basePath = PackageConstants.kIconsPath;
+  static const String _basePath = PackageConstants.kImagesPath;
 
-  static const String logo = '$_basePath/logo.svg';
-  static const String logoText = '$_basePath/logo_text.svg';
+  static const String logo = '$_basePath/logo.png';
 }
 
